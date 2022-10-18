dce89bc09a1322a2bb06a870cca77f4afef8a860
marcelovenagui <marcelovenancioaguiar@gmail.com>
2022-10-18 20:01:19 -0300

    Create ex013.js

---
+++ aula 05/ex013.js
@@ -0,0 +1,31 @@
+var agora = new Date()
+var DiaSem = agora.getDay()
+
+
+switch(DiaSem) {
+    case 0: console.log('Domingo')
+    break
+
+    case 1: console.log('Segunda')
+    break
+
+    case 3: console.log('Terça')
+    break
+
+    case 4: console.log('Quarta')
+    break
+
+    case 5: console.log('Quinta')
+    break
+
+    case 6: console.log('Sexta')
+    break
+
+    case 7: console.log('Sábado')
+    break
+
+    default:
+        console.log('Dia inválido')
+    break
+
+}
