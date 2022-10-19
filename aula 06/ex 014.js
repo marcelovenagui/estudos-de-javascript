15fa474343faa160c86efd4bc791c4745a0ebecf
marcelovenagui <marcelovenancioaguiar@gmail.com>
2022-10-19 16:09:28 -0300

    Update ex 014.js

--- aula 06/ex 014.js
+++ aula 06/ex 014.js
@@ -1,5 +1,20 @@
 var c = 1
-while (c <= 6){
-    console.log('Tudo bem ?')
+   do{
+    console.log(`Passo ${c}`)
     c++
-}
+} while(c <= 6)
+
+
+
+
+
+
+
+
+
+
+/*var c = 1
+    while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+} */
