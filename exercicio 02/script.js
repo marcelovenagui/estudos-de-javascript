2def771f1c6ac9b6501cd71ed27e9f94ec11e393
marcelovenagui <marcelovenancioaguiar@gmail.com>
2022-10-25 16:04:53 -0300

    Verificador de idades!!!

--- exercicio 02/script.js
+++ exercicio 02/script.js
@@ -1,1 +1,48 @@
-alert('Olá')
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano){
+        window.alert('[Erro] Verifique os dados e tente novamente!')
+    } else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
+        if(fsex[0].checked){
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'bebehomem.jpg')
+            } else if(idade < 21) {
+                //jovem
+                img.setAttribute('src', 'homemjovem.jpg')
+            } else if (idade <50){
+                //adulto
+                img.setAttribute('src', 'homemadulto.jpg')
+            } else{
+                //idoso
+                img.setAttribute('src', 'homemidoso.jpg')
+            }
+        } else if (fsex[1].checked){
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'bebemulher.jpg')
+            } else if(idade < 21) {
+                //jovem
+                img.setAttribute('src', 'mulherjovem.jpg')
+            } else if (idade <50){
+                //adulto
+                img.setAttribute('src', 'mulheradulta.jpg')
+            } else{
+                //idoso
+                img.setAttribute('src', 'mulheridosa.jpg')
+            }
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
