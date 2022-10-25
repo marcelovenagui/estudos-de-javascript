e033b004599e381423556be465848ac9658cb780
marcelovenagui <marcelovenancioaguiar@gmail.com>
2022-10-25 11:15:48 -0300

    Projeto hora do dia

--- exercicio 01/exercício 02/script.js
+++ exercicio 01/exercício 02/script.js
@@ -1,1 +1,20 @@
-alert('Olá')
+function carregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas!`
+    if ( hora >= 0 && hora < 12){
+        //Bom dia!
+        img.src = 'manha.jpg'
+        document.body.style.background = 'rgb(229, 212, 174)'
+    } else if (hora >= 12 && hora < 18){
+        //Boa tarde!
+        img.src = 'tarde.jpg'
+        document.body.style.background = 'rgb(231, 137, 75)'
+    } else {
+        // boa noite!
+        img.src = 'noite.jpg'
+        document.body.style.background = '#19283F'
+    }
+}
